429f7bcc70846cc93eccf83c6b929f3543eafc73
mshopov27 <m.shopov27@acsbg.org>
2026-03-05 18:20:23 +0200

    NextStatementIsALie

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,32 @@
 public class Character {
+    private String name;
+    private int suspicionLevel;
+    private boolean isKiller;
+    private boolean isAlive;
+
+    public Character(String name, boolean isKiller){
+        this.name = name;
+        this.isKiller = isKiller;
+        this.suspicionLevel = 0;
+        this.isAlive = true;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getSuspicionLevel(){
+        return suspicionLevel;
+    }
+    public void increaseSuspicionLevel(int amount){
+        suspicionLevel += amount;
+    }
+
+    public boolean getIsKiller(){
+        return isKiller;
+    }
+    public void setAlive(boolean alive){
+        this.isAlive = alive;
+    }
+
+
 }
--- src/GameState.java
+++ src/GameState.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class GameState {
+    private ArrayList<Character> characters;
+
+    public GameState(){
+        characters = new ArrayList<>();
+        Character mother = new Character("Mother", false);
+        Character father = new Character("Father", false);
+        Character olderSister = new Character("Older sister", false);
+        Character littleBrother = new Character("Little brother", false);
+        Character uncle = new Character("Uncle", false);
+        Character cousin = new Character("Cousin", false);
+        Character familyFriend = new Character("Family friend", false);
+
+        characters.add(mother);
+        characters.add(father);
+        characters.add(uncle);
+        characters.add(olderSister);
+        characters.add(littleBrother);
+        characters.add(uncle);
+        characters.add(cousin);
+        characters.add(familyFriend);
+    }
 }
