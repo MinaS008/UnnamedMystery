ab8c9cbd636d1b47d1e338766d467131d29e77dc
andreadelibeeva <a.delibeeva27@acsbg.org>
2026-03-06 17:35:04 +0200

    Update GameState.java
    the random killer logic

--- src/GameState.java
+++ src/GameState.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 
 public class GameState {
     private ArrayList<Character> characters;
+    private Character playableCharacter;
+    private Character killer;
+    private Random random;
 
     public GameState(){
         characters = new ArrayList<>();
+        random = new Random();
+        
         Character mother = new Character("Mother", false);
         Character father = new Character("Father", false);
         Character olderSister = new Character("Older sister", false);
@@ -18,8 +23,24 @@ public class GameState {
         characters.add(uncle);
         characters.add(olderSister);
         characters.add(littleBrother);
-        characters.add(uncle);
         characters.add(cousin);
         characters.add(familyFriend);
     }
+
+    public void setPlayableCharacter(Character player) {
+        this.playableCharacter = player;
+    }
+
+    public void chooseKiller() {
+        ArrayList<Character> possibleKillers = new ArrayList<>();
+        for(Character c: characters) {
+            if(!c.isChild() && c != playableCharacter) {
+                possibleKillers.add(c);
+            }
+        }
+        killer = possibleKillers.get(random.nextInt(possibleKillers.size()));
+        killer.setKiller(true);
+
+        System.out.println("DEBUG: Killer is: " + killer.getName());
+    } 
 }
