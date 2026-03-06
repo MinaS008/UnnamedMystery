00ad7c6a9bda95b2cd2ba3aa22cf74cbd39fc6b0
andreadelibeeva <a.delibeeva27@acsbg.org>
2026-03-06 17:45:14 +0200

    Update GameState.java
    NVM this is the randomization

--- src/GameState.java
+++ src/GameState.java
@@ -1,14 +1,11 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class GameState {
     private ArrayList<Character> characters;
-    private Character playableCharacter;
-    private Character killer;
-    private Random random;
 
     public GameState(){
         characters = new ArrayList<>();
-        random = new Random();
         
         Character mother = new Character("Mother", false);
         Character father = new Character("Father", false);
@@ -27,20 +24,20 @@ public class GameState {
         characters.add(familyFriend);
     }
 
-    public void setPlayableCharacter(Character player) {
-        this.playableCharacter = player;
+    public void assignRandomKiller(Character playerCharacter) {
+        Random rand = new Random();
+        Character killer;
+
+        do {
+            int index = rand.nextInt(characters.size());
+            killer = characters.get(index);
+        } while (killer == playerCharacter || killer.getName().equals("Little brother"));
+
+        killer.setKiller(true);
+        System.out.println("DEBUG: Killer is " + killer.getName());
     }
 
-    public void chooseKiller() {
-        ArrayList<Character> possibleKillers = new ArrayList<>();
-        for(Character c: characters) {
-            if(!c.isChild() && c != playableCharacter) {
-                possibleKillers.add(c);
-            }
-        }
-        killer = possibleKillers.get(random.nextInt(possibleKillers.size()));
-        killer.setKiller(true);
-
-        System.out.println("DEBUG: Killer is: " + killer.getName());
-    } 
+    public ArrayList<Character> getCharacters() {
+        return characters;
+    }
 }
