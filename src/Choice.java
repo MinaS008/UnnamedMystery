5e8c6631c3ea1fc91086f71c9776b3d79d2498e3
andreadelibeeva <a.delibeeva27@acsbg.org>
2026-03-08 14:44:02 +0200

    Update Choice.java

--- src/Choice.java
+++ src/Choice.java
@@ -5,17 +5,31 @@ public class Choice {
   private String requiredItem;
   private NextStatementIsALie.characterNames requiredCharacter;
   private NextStatementIsALie.characterNames requiredAliveCharacter;
-  private NextStatementIsALie.characterNames minSuspiciontarget;
+  private NextStatementIsALie.characterNames minSuspicionTarget;
   private int minSuspicionValue;
 
-  private Choise(Builder b) {
-    this.text = b.text;
-    this.targetSceneID = b.targetSceneID;
-    this.requiredItem = b.requiredItem;
-    this.requiredCharacter = b.requiredCharacter;
-    this.requiredAliveCharacter = b.requiredAliveCharacter;
-    this.minSuspicionTarget = b.minSuspicionTarget;
-    this.minSUspicionValue = b.minSuspicionValue;
+  public Choice (String text, String targetSceneID) {
+    this.text = text;
+    this.targetSceneID = targetSceneID;
+    this.requiredItem = null;
+    this.requiredCharacter = null;
+    this.requiredAliveCharacter = null;
+    this.minSuspicionTarget = null;
+    this.minSuspicionValue = 0;
+  }
+
+  public Choice(String text, String targetSceneID, String requiredItem,
+                  NextStatementIsALie.characterNames requiredCharacter,
+                  NextStatementIsALie.characterNames requiredAliveCharacter,
+                  NextStatementIsALie.characterNames minSuspicionTarget,
+                  int minSuspicionValue) {
+    this.text = text;
+    this.targetSceneID = targetSceneID;
+    this.requiredItem = requiredItem;
+    this.requiredCharacter = requiredCharacter;
+    this.requiredAliveCharacter = requiredAliveCharacter;
+    this.minSuspicionTarget = minSuspicionTarget;
+    this.minSuspicionValue = minSuspicionValue;
   }
 
   //Getters
@@ -45,46 +59,4 @@ public class Choice {
   public String toString() {
     return "> " + text + " -> [" + targetSceneID + "]";
   }
-
-  public static class Builder {
-    private String text;
-    private String targetSceneID;
-    private String requiredItem = null;
-    private NextStatementIsALie.characterNames requiredCharacter = null;
-    private NextStatementIsALie.characterNames requiredAliveCharacter = null;
-    private NextStatementIsALie.characterNames minSuspicionTarget = null;
-    private int minSuspicionValue = 0;
-
-  }
-
-  public Builder(String text, String targetSceneID) {
-    this.text = text;
-    this.targetSceneID = targetSceneID;
-  }
-
-  public Builder requireItem(String itemID) {
-    this.requiredItem = itemID;
-    return this;
-  }
-
-  public Builder requireCharacter(NextStatementIsALie.characterNames character) {
-            this.requiredCharacter = character;
-            return this;
-  }
-
-  public Builder requireAlive(NextStatementIsALie.characterNames character) {
-            this.requiredAliveCharacter = character;
-            return this;
-  }
-
-  public Builder requireSuspicion(NextStatementIsALie.characterNames target, int minimum) {
-            this.minSuspicionTarget = target;
-            this.minSuspicionValue  = minimum;
-            return this;
-  }
-
-  public Choice build() {
-            return new Choice(this);
-  }
-
 }
